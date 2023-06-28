091c1a628b599ba5645d63f92c028dddc9a60135
Thomas <tstuart804@gmail.com>
2023-06-28 09:20:16 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+# Web Scrape Practice
+import requests
+from bs4 import BeautifulSoup
+from random import choice
+from csv import DictReader
+
+BASE_URL = "http://quotes.toscrape.com"
+
+def read_quotes(filename):
+  with open(filename, "r") as file:
+    csv_reader = DictReader(file)
+    quotes = list(csv_reader)
+  return quotes
+  
+def start_game(quotes):
+  quote = choice(quotes)
+  print(quote["text"])
+  remaining_guesses = 4
+  guess = ''
+
+  while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+    guess = input(
+      f"Who said this quote?\nGuesses remaining:{remaining_guesses}\n>>>")
+    if guess.lower() == quote["author"].lower():
+      print("You got it right!")
+      break
+    remaining_guesses -= 1
+    if remaining_guesses == 3:
+      res = requests.get(f"{BASE_URL}{quote['bio-link']}")
+      soup = BeautifulSoup(res.text, "html5lib")
+      birth_date = soup.find(class_="author-born-date").get_text()
+      birth_place = soup.find(class_="author-born-location").get_text()
+      print(
+        f"Here's a hint: The author was born {birth_place} on {birth_date}")
+    elif remaining_guesses == 2:
+      print(
+        f"Here\'s a hint: The author\'s first name starts with: {quote['author'][0]}"
+      )
+    elif remaining_guesses == 1:
+      last_initial = quote["author"].split(" ")[1][0]
+      print(
+        f"Here\'s a hint: The author\'s last name starts with: {last_initial}")
+    else:
+      print(f"Sorry you ran out of guesses. The answer was {quote['author']}")
+
+  again = ''
+  while again.lower() not in ('y', 'n', 'yes', 'no'):
+    again = input("Would you like to play again (y/n)?\n")
+  if again.lower() in ('yes', 'y'):
+    return start_game(quotes)
+  else:
+    print('bye')
+
+
+quotes = read_quotes("quotes.csv")
+start_game(quotes)
